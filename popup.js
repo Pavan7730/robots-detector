20ef5d062f0dee151bf8c5be4ad1176902d4b878
Pavan7730 <82305384+Pavan7730@users.noreply.github.com>
2026-01-31 16:16:46 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-    if (!tab?.id) return;
+    if (!tab || !tab.id) {
+      document.getElementById("output").innerText = "No active tab";
+      return;
+    }
 
-    chrome.tabs.sendMessage(
-      tab.id,
-      { type: "GET_META" },
-      (metaRes) => {
-        chrome.runtime.sendMessage(
-          { type: "CHECK_ROBOTS", pageUrl: tab.url },
-          (robotsRes) => {
-            document.getElementById("output").innerText = `
+    chrome.tabs.sendMessage(tab.id, { type: "GET_META" }, (metaRes) => {
+      if (!metaRes) {
+        document.getElementById("output").innerText =
+          "Meta data not accessible.";
+        return;
+      }
+
+      chrome.runtime.sendMessage(
+        { type: "CHECK_ROBOTS", pageUrl: tab.url },
+        (robotsRes) => {
+          document.getElementById("output").innerText = `
 Meta Robots: ${metaRes.metaRobots}
 Canonical: ${metaRes.canonical}
 Robots.txt: ${robotsRes.robotsStatus}
 Non Follow / No Index: ${metaRes.noIndexNoFollow}
-            `.trim();
-          }
-        );
-      }
-    );
+          `.trim();
+        }
+      );
+    });
   });
 });
